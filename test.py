57e267caaa128378795c3803d90c3d0c12c0fe35
chasehensley <chaserhensley@gmail.com>
2014-07-28 16:55:48 -0400

    second

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print("hey")
+print("hello world")
+
+name = input ("what is your name?")
+  
+print("hello" + name)
+
+###########################################
+
+age = input ("what is your age?")
+
+age = int(age)
+
+days_in_age = 365.424 * age
+
+print("you are" + str(days_in_age))
+
