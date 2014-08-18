6ea7f565fd8ecb11bd7fcf271e4395f74b41034f
chasehensley <chaserhensley@gmail.com>
2014-08-18 17:20:38 -0400

    waffles
    waffles

---
+++ walffles.py
@@ -0,0 +1,10 @@
+answer_=_input("do you like waffles?")
+
+
+if answer_==_ 'yes':
+   print(" Yeah we like waffles")
+elif_answer_=='waffles are yucky':
+    print("you're rude!")
+else:
+    print("WELL WE DO AND GET OUT OF HERE NOW NOW NOW NOW NOW NOW NOW NOW OR YOUR DEAD HAHAHAHAHAHAHAHAHAHA")
+    
