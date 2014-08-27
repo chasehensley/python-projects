36a120f477fe3eb52e1d8ddcfdf6fac4f641b980
chasehensley <chaserhensley@gmail.com>
2014-08-27 05:55:59 -0400

    python
    python

--- python-yes-no.py
+++ python-yes-no.py
@@ -1,15 +1,4 @@
-answer_=input("Do you like python????")
-
-
-if answer_=='yes':
-    print("You must because you are using it :)")
-
-
-if answer_=='hi':
-    print("hey")
-
-else:
-     print("Well why are you using it?")
+print("hello rob")
 
 
 
