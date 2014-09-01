e558ce9e8dc5395d3c0de400cd027adc4aab7130
chase hensley <chaserhensley@gmail.com>
2014-09-01 17:18:09 -0400

    skilstak1
    skilstak1

---
+++ SkilBucks1.py
@@ -0,0 +1,33 @@
+import json
+
+rewards = [
+    {
+       'name': "Regular Attendance",
+       'desc': "One regular class (90-120 mins)",
+       'min': 40,
+       'max': 40,     
+    },
+    {
+        'name': "GitHub Green Dot",
+        'desc': "Get a green dot from committing something new.",
+        'min': 10,
+        'max': 40,
+        
+    }
+    
+ ]
+
+
+items = [
+    {
+        'name':"Tee shirt (Basic)",
+        'desc': "The basic SkilStak logo on a tee shirt (whatever size)."
+        'cost': 600
+    }
+    
+]
+
+
+for the reward in rewards:
+    if reward['min'] == 
+    
