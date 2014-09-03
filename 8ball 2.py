e9bd776e1ce100b556f6ecce633b219fe1df7487
chasehensley <chase@Scotts-MacBook-Air.local>
2014-09-02 20:06:16 -0400

    8ball 2
    8ball 2

---
+++ 8ball 2.py
@@ -0,0 +1,14 @@
+import random
+
+answers = [
+    'yes','no','maybe','pickles','Callie','SHUT UP','computer is shutting down','computer is broken now','you are being log off and never allowed on again',]
+
+
+while True:
+    print("whats is your question")
+    question = input('>')
+
+
+    answer = random.choice(answers)
+    print(answer)
+
