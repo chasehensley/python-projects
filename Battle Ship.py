37337dd3f1342c886d4bb290d3ba201c08091ad7
chase hensley <chaserhensley@gmail.com>
2014-09-08 16:49:01 -0400

    Battle Ship
    Battle Ship

---
+++ Battle Ship.py
@@ -0,0 +1,43 @@
+class Ship:
+    size = None
+
+    
+    def __init__(self):
+        self.front = None
+        slef.back = None
+        slef.destroy = False
+
+
+        def place(self,front,back):
+            self.format = front
+            self.format = back
+
+class Carrier(Ship):
+    size = 5
+
+class Battleship(Ship):
+    size = 4
+
+class Submarine(Ship):
+    size = 3
+
+class Destroyer(Ship):
+    size = 3
+    
+class PatrolBoat(Ship):
+    size = 2
+
+
+my_carrier = Carrier()
+comp_carrier = Carrier()
+
+
+while front not in
+front_row = input('Carrier Front Rowv[a-j]:
+back_col =input('Carrier Front Column[1-10]:
+
+
+    
+        
+
+
