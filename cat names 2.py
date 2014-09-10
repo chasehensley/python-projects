a54874f0befc4d1696391e9d70fa913fe5237625
chase hensley <chaserhensley@gmail.com>
2014-09-09 22:04:13 -0400

    cat names 2
    cat names 2

---
+++ cat names 2.py
@@ -0,0 +1,39 @@
+import time
+
+
+chase_list = ['callie', 'princess', 'tiger', 'hokie', 'spot', 'leo']
+
+
+print ("Cat names")
+
+
+time.sleep(3)
+
+
+print(chase_list)
+
+
+time.sleep(3)
+
+
+print("favorite cat name")
+
+
+time.sleep(3)
+
+
+print(chase_list[0])
+
+
+time.sleep(3)
+
+
+print("Least favorite cat name")
+
+
+time.sleep(3)
+
+
+print(chase_list[1])d
+
+
