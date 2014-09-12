1020bdd96d80f0e68422e3bc924cf42252029825
chase hensley <chaserhensley@gmail.com>
2014-09-12 16:44:20 -0400

    cat names 5
    ctat names 5

---
+++ cat names 5.py
@@ -0,0 +1,67 @@
+import time
+
+
+chase_list = ['callie', 'princess', 'tiger', 'hokie', 'spot', 'leo']
+
+
+print ("Cat names")
+
+
+time.sleep(3)
+
+
+print(chase_list)
+
+
+time.sleep(3)
+
+
+print("favorite cat name")
+
+
+time.sleep(3)
+
+
+print(chase_list[0])
+
+
+time.sleep(3)
+
+
+print("Least favorite cat name")
+
+
+time.sleep(3)
+
+
+print(chase_list[1])
+
+
+time.sleep(3)
+
+
+print("I cant think any more")
+
+
+print("Middle Cat name")
+
+
+time.sleep(3)
+
+
+print(chase_list[4])
+
+
+time.sleep(3)
+
+
+print("Ok cat name")
+
+
+time.sleep(3)
+
+
+print(chase_list[3])
+
+
+
