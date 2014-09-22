7d7263eca34d9c64150b439f74009a380cf1304c
chase hensley <chaserhensley@gmail.com>
2014-09-22 17:23:42 -0400

    chase's song

---
+++ Chase's Song.py
@@ -0,0 +1,13 @@
+import time
+
+
+
+print("Chase's Song")
+time.sleep(1)
+print("~~~~~~~~~~~~~~~~~~~~~~")
+time.sleep(1)
+print("THIS IS CHASES SONG!!! IT GOES ON ALL DAY LONG!!!!!!! LONG LONG CHASES SONG!!!!!!")
+time.sleep(0.5)
+print("YO, MY NAME IS CHASE, I DONT LIKE TO JACE, BUT I AINT NO HACE, IMMMMM CHAASSSSE YYYAAAA")
+time.sleep(10)
+print("chase's sonnnnggggggg...... it's chases songggggg........ DA END :P")
