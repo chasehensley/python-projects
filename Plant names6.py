4d06a6dfed847dbba75fc389133aec9b8fc8411c
chase hensley <chaserhensley@gmail.com>
2014-09-28 14:54:41 -0400

    plant names 6

---
+++ Plant names6.py
@@ -0,0 +1,35 @@
+import random
+import time
+
+
+name = ['Fred','George','Gorge','Joe','Chase the amazing','jeff','jeffery','niko','rob','Bob','pete the palm','sam','greg','tree','spot the pot','Mr.plant','Pokey','michel','david','corn','rocco','Uhhh...... TRIANGE???!!!! D:','jack','jason','chair','leafy','trupet','{}','hey','bob','ROBROBROBMUH',]
+
+print("Finding plant name....... Finding plant name....")
+time.sleep(2)
+print("Found plant name:")
+time.sleep(1)
+plant_name = random.choice(name)
+print(plant_name)
+print("--------------------------------------------- THATS THE NAME TROOLLLLLLLLLOLOLOLOLOLOLOLOLOLOLOL LIKE IT YOU MUST NOW NOWN NOW NOW PlANT NAMES")
+
+
+
+time.sleep(5)
+      
+
+
+
+
+
+
+while True:
+    print("Finding plant name..... Finding plant name....")
+    time.sleep(4)
+    print("Found plant name:")
+    time.sleep(1)
+    plant_name = random.choice(name)
+    print(plant_name)
+    print("------------------")
+
+ 
+    
