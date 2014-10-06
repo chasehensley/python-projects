cc832b81f4fe7c979a73b1068ab0654bbab0799f
chase hensley <chaserhensley@gmail.com>
2014-10-06 17:14:24 -0400

    skilstak 2

---
+++ SkilBucks2.py
@@ -0,0 +1,11 @@
+buxfor = {}
+buxfor['onehourclass'] = 1
+buxfor['fourregclass'] = buxfor['onehourclass']*1.5*4
+buxfor['greendot-1day']=1
+buxfor['greendor4weeks'] = buxfor['greendot-1day']*7*4
+
+buxfor['normal-year'] =buxfor['normal-4weeks']*13
+
+
+print(buxfor)
+   
