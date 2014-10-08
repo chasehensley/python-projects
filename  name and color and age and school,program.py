1fc00a1826aad85a2cd142ca4caf34baf17fda5c
chase hensley <chaserhensley@gmail.com>
2014-10-08 16:00:17 -0400

    and program

---
+++  name and color and age and school,program.py
@@ -0,0 +1,19 @@
+name=input("what is you name?")
+print(name)
+
+
+color=input("what is your favoite color?")
+print(color)
+
+
+age=input("what is your age?")
+print(age)
+
+
+school=input("what shcool do you go to?")
+print("you go to")
+print(school)
+
+
+program=input("do you like to program?")
+print(program)
