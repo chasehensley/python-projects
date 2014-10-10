fbcbc150e8e5454cb818e0b88d7161fcd2d4dc8c
chase hensley <chaserhensley@gmail.com>
2014-10-10 19:20:19 -0400

    chase

---
+++ chase.py
@@ -0,0 +1,27 @@
+name=input("what is you name?")
+print(name)
+
+
+color=input("what is your favoite color?")
+print(color)
+
+
+age=input("what is your age?")
+print(age)
+
+
+school=input("what shcool do you go to?")
+print("you go to")
+print(school)
+
+
+program=input("do you like to program?")
+print(program)
+
+
+rob=input("Do you like rob?")
+print(rob)
+
+
+chase=input("Do you like chase?")
+print(chase)
