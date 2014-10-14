3472af5ff04a1df522111ab76822b680772709d8
chase hensley <chaserhensley@gmail.com>
2014-10-14 18:53:00 -0400

    skilstak

---
+++ skilstak.py
@@ -0,0 +1,39 @@
+name=input("what is you name?")
+print(name)
+
+
+color=input("what is your favoite color?")
+print(color)
+
+
+age=input("what is your age?")
+print(age)
+
+
+school=input("what shcool do you go to?")
+print("you go to")
+print(school)
+
+
+program=input("do you like to program?")
+print(program)
+
+
+rob=input("Do you like rob?")
+print(rob)
+
+
+chase=input("Do you like chase?")
+print(chase)
+
+
+stacy=input("Do you like my stacy")
+print(stacy)
+
+
+scott=input("Do you like stacy?")
+print(stacy)
+
+
+print("do you like skilstak?")
+print(skilstak)
