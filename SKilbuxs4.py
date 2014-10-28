fcc0eef1485e410d123b4ef7a3440296b9f3a44e
chase hensley <chaserhensley@gmail.com>
2014-10-28 17:26:06 -0400

    skilbuxs4

---
+++ SKilbuxs4.py
@@ -0,0 +1,20 @@
+buxperclass = 2
+buxpergreendot = 1
+buxperport = 2
+buxperhourhourofwork = 10
+buxpercodeorg = 10
+
+buxpergreendot=input("How many green dots do you have?")
+print(buxpergreendot)
+print("You have ^ SkilBuxs from GreenDots")
+
+buxforcodeorg=input("Have you Finished code.org? yes or no Please make sure its lower case")
+print(buxforcodeorg)
+if answer == 'yes':
+    print("You earned 30 SkilBuxs beacause you finished code.org")
+
+else answer == 'no':
+    print("Sorry everyone sad ;( me too")
+
+buxfperhourofwork=input("Have you done a hour of work on the server (Sorry Buy this question is only for admins)")
+print(buxperhourofwork)
