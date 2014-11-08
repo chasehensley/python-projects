5bbc925d167c3fe91e6a0f45bc8f0bdd9959637b
chase hensley <chaserhensley@gmail.com>
2014-11-07 22:54:55 -0500

    hours 2

--- hours.py
+++ hours.py
@@ -2,9 +2,9 @@ mins_in_hour = input("How many hours of minecraft have you played?")
 
 
 
-mins_in_hour * 60
+played = mins_in_hour * 60
 
-print(mins_in_hour)
+print(played)
 print("you played for ^ minutes")
 
 
