1f9e7d9fafcb98832471fef204748dac88e4e02d
chase hensley <chaserhensley@gmail.com>
2014-12-22 16:36:54 -0500

    quiz game

--- Teaching.py
+++ Teaching.py
@@ -1,7 +1,16 @@
-input random
+
 
 def question1():
     q1 = input(''' Can pigs swim? yes or no.''')
-    if answer = 'yes':
+    if answer 'yes':
         print("Correct!")
+
+def question2():
+    q2 = input(''' How many points does a snowflake have?''')
+    if answer '6':
+        print("correct!")
     
+def question3():
+    q3 = input(''' Who wrote How the Grinch Stole Christmas?''')
+    if answer 'dr.suess':
+        print("correct!")
